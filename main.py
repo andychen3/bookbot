14eac8e28978b3b3713b6d51d04278fb4e44ae75
Andy Chen <andychen@andys-mbp.lan>
2024-01-14 17:57:27 -0500

    Created file to parse words from document.

---
+++ main.py
@@ -0,0 +1,17 @@
+def main():
+    path_to_file = "/Users/andychen/workspace/bookbot/books/frankenstein.txt"
+    file_contents = open_file(path_to_file)
+    print(f"There are {get_count(file_contents)} words in this document.")
+
+
+def get_count(file):
+    return len(file.split())
+
+
+def open_file(path):
+    with open(path) as f:
+        return f.read()
+
+
+if __name__ == "__main__":
+    main()
