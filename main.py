3361b750e4caeb1166b078c9debf3f490a2d4f65
Andy Chen <andychen@andys-mbp.lan>
2024-01-14 18:05:24 -0500

    Created function to count numbers of letter in file.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     path_to_file = "/Users/andychen/workspace/bookbot/books/frankenstein.txt"
     file_contents = open_file(path_to_file)
     print(f"There are {get_count(file_contents)} words in this document..")
+    get_letters(file_contents)
 
 
 def get_count(file):
@@ -11,6 +12,20 @@ def get_count(file):
 def open_file(path):
     with open(path) as f:
         return f.read()
+
+
+def get_letters(file):
+    letter_count_dict = {}
+
+    for char in file:
+        lowercase_letter = char.lower()
+        if lowercase_letter not in letter_count_dict:
+            letter_count_dict[lowercase_letter] = 1
+        else:
+            letter_count_dict[lowercase_letter] += 1
+
+    for key, val in letter_count_dict.items():
+        print(f"{key}: {val}")
 
 
 if __name__ == "__main__":
