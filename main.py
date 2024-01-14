eaa826b48163c549274b82024839a7ec5d5cb3ff
Andy Chen <andychen@andys-mbp.lan>
2024-01-14 18:24:07 -0500

    Created generate report function to create report listing number of characters in it.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
     path_to_file = "/Users/andychen/workspace/bookbot/books/frankenstein.txt"
     file_contents = open_file(path_to_file)
-    print(f"There are {get_count(file_contents)} words in this document..")
-    get_letters(file_contents)
+    word_count = get_count(file_contents)
+    sorted_list = get_letters(file_contents)
+    generate_report(word_count, sorted_list, path_to_file)
 
 
 def get_count(file):
@@ -24,8 +25,19 @@ def get_letters(file):
         else:
             letter_count_dict[lowercase_letter] += 1
 
-    for key, val in letter_count_dict.items():
-        print(f"{key}: {val}")
+    new_list = sorted(letter_count_dict.items(), key=lambda x: x[1], reverse=True)
+    return new_list
+
+
+def generate_report(word_count, sorted_list, path_to_file):
+    report_title = path_to_file.split("/")[-1]
+    print(f"--- Begin report of document {report_title} ---")
+    print(f"{word_count} words found in the document")
+    print()
+    for word, count in sorted_list:
+        if word.isalpha():
+            print(f"The '{word}' character was found {count} times")
+    print("--- End report ---")
 
 
 if __name__ == "__main__":
