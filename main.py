0bae18bdd324e26696af6516ab9d695c649ada76
Andy Chen <andychen@andys-mbp.lan>
2024-01-14 17:58:56 -0500

    Test.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     path_to_file = "/Users/andychen/workspace/bookbot/books/frankenstein.txt"
     file_contents = open_file(path_to_file)
-    print(f"There are {get_count(file_contents)} words in this document.")
+    print(f"There are {get_count(file_contents)} words in this document..")
 
 
 def get_count(file):
